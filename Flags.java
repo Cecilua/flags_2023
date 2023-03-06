0a4e59531deca53a311bfab6a8f9fce8d272bb7a
Cecilua <65154517+Cecilua@users.noreply.github.com>
2023-03-06 19:11:24 +1300

    horizontal three striped flag

--- Flags.java
+++ Flags.java
@@ -10,6 +10,7 @@ public class Flags
 {
     // instance variables 
 
+
     /**
      * Constructor for objects of class Flags
      */
@@ -17,42 +18,105 @@ public class Flags
     {
         // initialise instance variables
         UI.initialise(); // bring up ecs100 canvas
-        UI.addButton("flag time B-)", this::drawFlag);
+        UI.addButton("flag time B-)", this::drawFlags);
         UI.addButton("bye bye~", UI::quit); 
     }
 
-    public void drawFlag() {
-        // draw thai flag 
+    /**
+     * draw 3 stripe flag 
+     * here the stripes will be the same size 
+     */
+    public void doDrawThreeStripeFlagY(double left, double top, double width, double height) {
+        // test
+        final int LINEWIDTH = 2;
         
-        // for each stripe 
-            // horizontal position (left) 
-            // vertical position (top)
-            // lenght (width) 
-            // height 
-            
-        // for horizontal flags, the left position of all stripes is the same
-        // with the vertical being under the previous stripe 
-        // vertical flags is the opposite 
+        // draw vertical flag
+        //  stripe 1 
+        UI.setColor(Color.red);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, top, width, height);
+        
+        left += width;  
+        
+        //  stripe 2 
+        UI.setColor(Color.white);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, top, width, height);
+        
+        left += width;
+        
+        // stripe 3 
+        UI.setColor(Color.blue);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, top, width, height);
+        
+        } 
+    
+    public void doDrawThreeStripesX(double left, double top, double width, double height, Color color1, Color color2, Color color3) {
+        // linewidth is a constant 
+        final int LINEWIDTH = 2;
+        
+        // horizontal stripe 1 
+        UI.setColor(color1);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, top, width, height);
+        
+        top += height; 
+        
+        // horizontal stripe 2 
+        UI.setColor(color2);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, 200, width, height);
+        
+        top += height;
+        
+        // horizontal stripe 3 
+        UI.setColor(color3);
+        UI.setLineWidth(LINEWIDTH);
+        UI.fillRect(left, top, width, height);
+
+        }
+    
+    public void drawFlag() {
+        int left = 0;
+        int top = 0;
+        int width = 2000;
+        int height = 100; 
         
         UI.setColor(Color.red);
         UI.setLineWidth(2);
-        UI.fillRect(0, 0, 200, 100);
+        UI.fillRect(left, top, width, height);
+        
+        top += height;
         
         UI.setColor(Color.white);
         UI.setLineWidth(2);
-        UI.fillRect(0, 100, 2000, 100);
+        UI.fillRect(left, top, width, height);
+        
+        top += height;
         
         UI.setColor(Color.blue);
         UI.setLineWidth(2);
-        UI.fillRect(0, 200, 2000, 200);
+        UI.fillRect(left, top, width, height);
+        
+        top += height;
         
         UI.setColor(Color.white);
         UI.setLineWidth(2);
-        UI.fillRect(0, 400, 2000, 100);
+        UI.fillRect(left, top, width, height);
+        
+        top += height;
         
         UI.setColor(Color.red);
         UI.setLineWidth(2);
-        UI.fillRect(0, 500, 2000, 100);
+        UI.fillRect(left, top, width, height);
         
     }
+    
+    public void drawFlags(){
+        //doDrawThreeStripeFlagY(0, 100, 200, 100);
+        doDrawThreeStripesX(0, 0, 2000, 200, Color.red, Color.white, Color.blue);
+        
+        //drawFlag();
+    } 
 }
